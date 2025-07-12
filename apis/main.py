f8b11236bcec7ebcc5b2fb1aa74ecdb4b5b2df6c
Rahul Jangid <112081189+rahul-jerthi@users.noreply.github.com>
2025-07-12 22:41:35 +0530

    Update main.py

--- apis/main.py
+++ apis/main.py
@@ -110,5 +110,5 @@ async def predict(file: UploadFile = File(...)):
     #     "confidence": confidence
     # }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
